c7897f046880e63037f46b73d811951fd412a3d7
Minjea Kim <jbm98@naver.com>
2023-03-15 17:51:35 +0900

    making assetmanager

---
+++ src/AssetManager.hpp
@@ -0,0 +1,23 @@
+#ifndef ASSETMANAGER_HPP
+#define ASSETMANAGER_HPP
+
+#include <map>
+#include <string>
+#include "TextureManager.hpp"
+#include "Vector2D.hpp"
+#include "ECS/ECS.hpp"
+#include "SDL2/SDL_ttf.h"
+
+class AssetManager {
+public:
+    AssetManager(Manager *man);
+    ~AssetManager();
+
+
+    
+private:
+}
+
+
+
+#endif
